d5a1a5cb510328cbd24322b019cd661bc35edd8a
piyi-juanjis <94360229+piyi-juanjis@users.noreply.github.com>
2021-11-23 20:01:36 +0100

    Add files via upload

---
+++ holamundo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class holamundo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("escriba hola mundo");
+		String frase = sc.next();
+		System.out.println(frase);
+		
+
+	}
+
+}
